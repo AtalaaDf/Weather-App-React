f13c617399514498cd297ea5956c094a4cfea63c
AtalaaDf <mataladaffaalfaristi@gmail.com>
2026-05-14 10:53:23 +0700

    feat: search button with dialog, and adjust the swith theme button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,18 @@
 import {NavigationMenu, NavigationMenuList, 
         NavigationMenuItem, NavigationMenuLink} from '@/components/ui/navigation-menu'
+import {CommandDialog, CommandInput, CommandList, 
+        CommandEmpty, CommandGroup, CommandItem} from '@/components/ui/command'
 import { Switch } from '@/components/ui/switch'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from '@/components/ui/button'
+import { Command } from '@/components/ui/command'
 
 function Navbar(){
-    const [isDark, setIsDark] = useState(false)
+    const [isDark, setIsDark] = useState(
+        localStorage.getItem('theme') === 'true'
+    )
+    const [isOpen, setOpen] = useState(false)
 
     function onCheckedChange(value){
 
@@ -17,9 +24,22 @@ function Navbar(){
             document.documentElement.classList.remove('dark')
         }
 
+        localStorage.setItem('theme', value)
+        
     }
 
+    useEffect(()=>{
+        const saveTheme =localStorage.getItem('theme')
+        if (saveTheme == 'true') {
+            setIsDark(true)
+            document.documentElement.classList.add('dark');
+        }
+        },[])
+
+
     return(
+        <>
+
         <nav className="sticky top-0 backdrop-blur-sm border-b flex
         justify-between items-center px-6 py-3">
         <h1 className='text-blue-500 font-bold text-xl'>⛅Weather App</h1>
@@ -27,15 +47,36 @@ function Navbar(){
         <NavigationMenu>
             <NavigationMenuList>
                 <NavigationMenuItem>
-                    <NavigationMenuLink>
+                    <NavigationMenuLink asChild>
                         <Link to="/">Home</Link>
                     </NavigationMenuLink>
                 </NavigationMenuItem>
             </NavigationMenuList>
         </NavigationMenu>
 
-        <Switch onCheckedChange={onCheckedChange}/>
+        <Button onClick={()=> setOpen(true)}>
+        ⋱⋱ Cari kota
+        </Button>
+
+        <Switch onCheckedChange={onCheckedChange} checked={isDark}/>
+
         </nav>
+
+        <CommandDialog open={isOpen} onOpenChange={setOpen}>
+            <Command>
+                    <CommandInput />
+                <CommandList>
+                    <CommandEmpty>Tidak ditemukan di pencarian.</CommandEmpty>
+                        <CommandGroup>
+                            <CommandItem>Jakarta</CommandItem>
+                            <CommandItem>Palembang</CommandItem>
+                        </CommandGroup>
+                </CommandList>
+            </Command>
+        </CommandDialog>
+
+
+    </>
     )
 }
 export default Navbar
